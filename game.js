bdbc680ffad83295bb3bbca58c74dd56ebb060c4
j-kemble <168929762+j-kemble@users.noreply.github.com>
2025-02-02 01:14:02 -0600

    start js

---
+++ game.js
@@ -0,0 +1,6 @@
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+const resultDisplay = document.getElementById('round_winner');
+const gameResult = document.getElementById('game_winner');
+
