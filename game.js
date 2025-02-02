8d9f710032e7cfa16f4e8967748ce3e7201e7e41
j-kemble <168929762+j-kemble@users.noreply.github.com>
2025-02-02 03:03:07 -0600

    make a few changes to html, css and js.

--- game.js
+++ game.js
@@ -1,6 +1,15 @@
-const rockButton = document.getElementById('rock');
-const paperButton = document.getElementById('paper');
-const scissorsButton = document.getElementById('scissors');
-const resultDisplay = document.getElementById('round_winner');
-const gameResult = document.getElementById('game_winner');
+const playerCountElement = document.querySelector('.player_count');
+const computerCountElement = document.querySelector('.computer_count');
+const remainingMoves = document.querySelector('.moves')
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+const roundWinner = document.querySelector('#round_winner');
+const gameResult = document.querySelector('#game_winner');
+let playerCount = 0;
+let computerCount = 0;
+let movesRemaining = 10;
 
+function getComputerChoice(choice) {
+
+}
